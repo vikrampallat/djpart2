47a59c98b73f7d669989234e1be3f7c7cc35530b
vikrampallat <76404144+vikrampallat@users.noreply.github.com>
2021-08-14 13:44:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song = "";
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function draw() {
+    image(video, 0, 0, 500, 500, );
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        
+        console.log("rightWristX = " + rightWristX +" rightWristY"+ rightWristY);
+    }
+}
